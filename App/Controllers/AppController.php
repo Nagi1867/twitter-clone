8a5884233c7cccaef78ac782060984fdbfbebb4c
Gabriel Patano Kiyot <gabrielkiyoto@unifev.corp.br>
2025-08-18 11:58:36 -0300

    timeline listando tweets

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -9,11 +9,40 @@
         public function timeline() {
             session_start();
 
+            $this->validaAutenticacao();
+
+            
+                $tweet = Container::getModel('Tweet');
+                $tweet->__set('id_usuario', $_SESSION['id']);
+                $tweets = $tweet->getAll();
+
+                $this->view->tweets = $tweets;
+
+                $this->render('timeline');                   
+        }
+
+        public function tweet() {
+            session_start();
+
             if($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
-                $this->render('timeline');
+                $tweet = Container::getModel('Tweet');
+
+                $tweet->__set('tweet', $_POST['tweet']);
+                $tweet->__set('id_usuario', $_SESSION['id']);
+
+                $tweet->salvar();
+
+                header('Location: /timeline');
             } else {
                 header('Location: /?login=erro');
-            }                        
+            }  
+        }
+
+        public function validaAutenticacao() {
+            session_start();
+            if(!isset($_SESSION['id']) || $_SESSION['id'] != '' || !isset($_SESSION['nome']) || $_SESSION['id'] != '') {
+                header('Location: /?login=erro');
+            }
         }
     }
 ?>
