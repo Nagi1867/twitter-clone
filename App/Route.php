3703c67c885a2c79ef0abc4e465e582d23c027ef
Gabriel Patano Kiyot <gabrielkiyoto@unifev.corp.br>
2025-08-18 09:58:12 -0300

    registrando usuarios

--- App/Route.php
+++ App/Route.php
@@ -19,6 +19,11 @@ class Route extends Bootstrap {
 			'controller' => 'indexController',
 			'action' => 'inscreverse'
 		);
+		$routes['registrar'] = array(
+			'route' => '/registrar',
+			'controller' => 'indexController',
+			'action' => 'registrar'
+		);
 
 		$this->setRoutes($routes);
 	}
