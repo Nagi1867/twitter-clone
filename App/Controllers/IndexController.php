508843d7236463bc358333307f3dcc2d2f3f61d6
Gabriel Patano Kiyot <gabrielkiyoto@unifev.corp.br>
2025-08-18 10:50:43 -0300

    autenticação de usuários

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -11,6 +11,8 @@ use MF\Model\Container;
 class IndexController extends Action {
 
 	public function index() {
+		$this->view->login = isset($_GET['login']) ? $_GET['login'] : '';
+
 		$this->render('index');
 	}
 
--- App/Models/Usuario.php
+++ App/Models/Usuario.php
@@ -48,6 +48,7 @@
             return $valido;
         }
 
+        //recuperar  um usuário por email
         public function getUsuarioPorEmail() {
             $query = 'select nome, email from usuarios where email = :email';
             $stmt = $this->db->prepare($query);
@@ -57,6 +58,22 @@
             return $stmt->fetchAll(\PDO::FETCH_ASSOC);
         }
 
-        //recuperar  um usuário por email
+        public function autenticar() {
+            $query = 'select id, nome, email from usuarios where email = :email and senha = :senha';
+
+            $stmt = $this->db->prepare($query);
+            $stmt->bindValue(':email', $this->__get('email'));
+            $stmt->bindValue(':senha', $this->__get('senha'));
+            $stmt->execute();
+
+            $usuario = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+            if($usuario['id'] != '' && $usuario['nome'] != '') {
+                $this->__set('id', $usuario['id']);
+                $this->__set('nome', $usuario['nome']);
+            }
+
+            return $this;
+        }
     }
 ?>
